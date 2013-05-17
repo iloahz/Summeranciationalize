7767dff369d471e2772444f20375afb76b20b758
Li Zhao <iloahz@gmail.com>
2013-05-17 23:06:36 +0800

    add allowed_hosts

--- Summeranciationalize/settings.py
+++ Summeranciationalize/settings.py
@@ -2,9 +2,10 @@
 import os
 
 PROJECT_DIR = os.getcwd()
-LOCAL = False
 if ':' in PROJECT_DIR:
     LOCAL = True
+else:
+    LOCAL = False
 if LOCAL:
     DOMAIN = '127.0.0.1:8000'
 else:
@@ -37,7 +38,7 @@ DATABASES = {
 
 # Hosts/domain names that are valid for this site; required if DEBUG is False
 # See https://docs.djangoproject.com/en/1.5/ref/settings/#allowed-hosts
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [DOMAIN]
 
 # Local time zone for this installation. Choices can be found here:
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
